155f8b9018f746f9f0b3978e8dbe88384eac342a
Rachel Dooley <102691550+Rae1821@users.noreply.github.com>
2023-05-26 13:04:44 -0400

    Fixed spacing issue in events section

--- js/main.js
+++ js/main.js
@@ -41,7 +41,7 @@ const btn = document.getElementById('btn-to-top');
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
         btn.style.display = "block";
     } else {
         btn.style.display = "none";
